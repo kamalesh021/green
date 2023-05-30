150e71341fb77d9c1913067b2663fb6997114b62
kamalesh <kamaleshkumar@02126>
2023-05-30 15:46:12 +0530

    gowtham conflicts resolve

--- src/org/india/India.java
+++ src/org/india/India.java
@@ -1,10 +1,13 @@
-package org.india;
+package  org.india;
 
 import org.tamilnadu.TamilNadu;
 
 public class India extends TamilNadu {
 public void india() {
 	System.out.println("india's over populated county");
+}
+private void india rank () {
+	System.out.println(" india rank is  no 1");
 }
 public static void main(String[] args) {
 	India i=new India();
@@ -13,7 +16,5 @@ public static void main(String[] args) {
 	i.malayalam();
 	i.telugu();
 }
-private void india rank () {
-	System.out.println(" india rank is  no 1");
 }
-}
+
